20275c39c75dcd4b310df8a4aa5ecc09b5191139
David Vaughan <dvaug86@gmail.com>
2024-01-05 12:29:59 -0500

    clean up files

---
+++ src/firebase.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyASs9wceMaWZ6ZM4zVnm_31a3hiuvYnJW0",
+  authDomain: "codecomm-auth.firebaseapp.com",
+  projectId: "codecomm-auth",
+  storageBucket: "codecomm-auth.appspot.com",
+  messagingSenderId: "1045208272118",
+  appId: "1:1045208272118:web:fd104bc3bdaf26bf3ab0d7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
